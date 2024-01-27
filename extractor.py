2bc0efd2a97ebee213aa81a0783b91933914a4da
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2024-01-26 22:53:20 -0300

    :sparkles: feat: feito tratamento de erros

--- extractor.py
+++ extractor.py
@@ -1,14 +1,23 @@
 import re
+import os
+from tkinter import messagebox
 
-with open(r"C:\Users\vitor\Desktop\mentoria\extrator-ip\ip-mockado.txt") as ipDoc:
-    ipRaw = ipDoc.readlines()
-    
-txtFile = open("listaIPs.txt","w+")
-    
-schema = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})') 
+if(os.path.exists(r".\lista-ips.txt")):
+    with open(r".\lista-ips.txt") as ipDoc:
+        ipRaw = ipDoc.readlines()
+        
+    if(os.path.exists(r".\IPs-tratados.txt")):
+        os.remove("IPs-tratados.txt")    
 
-for line in ipRaw:
-    ip = re.findall( schema, line )
-    if ip:
-        for i in ip:
-            txtFile.write(i + "\n")
+    txtFile = open("IPs-tratados.txt","w+")
+        
+    schema = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})') 
+
+    for line in ipRaw:
+        ip = re.findall( schema, line )
+        if ip:
+            for i in ip:
+                txtFile.write(i + "\n")
+else:
+    messagebox.showerror("Erro", "Favor criar um arquivo de texto(.txt) chamado 'lista-ips' no mesmo diretório deste aplicativo")
+    
